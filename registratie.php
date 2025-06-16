54748e68248b20c05ac1527bf076ef082daa8375
bsiebers <bwsiebers@gmail.com>
2025-06-16 15:47:48 +0200

    aanpassing styling en opties login+registratie

---
+++ registratie.php
@@ -0,0 +1,16 @@
+<?php
+$pdo = new PDO('mysql:host=localhost;dbname=testdb');
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$stmt = $pdo->prepare('SELECT * FROM users WHERE username = ?');
+$stmt->execute([$username]);
+$user = $stmt->fetch();
+
+if($user && password_verify($password , $user['password'])) {
+    echo "Ingelogd!";
+}else {
+    echo "Foutieve gebruikersnaam of wachtwoord.";
+}
+?>
